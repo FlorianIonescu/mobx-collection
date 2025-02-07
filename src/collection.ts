98bfe516ca290e4a5da8a801a3a2d7e9da7611e7
Florian Ionescu <go@antidefault.net>
2025-02-07 11:53:17 +0100

    collection only has a single registration map for every new predicate

--- src/collection.ts
+++ src/collection.ts
@@ -2,50 +2,51 @@ import { ObservableSet, transaction } from "mobx"
 import Item from "./item.js"
 
 type InputPredicate<T> = (value: T, props: boolean[]) => boolean
-type ItemPredicate<T> = (value: Item<T>) => boolean
+type Registration<T> = {
+  set: ObservableSet<T>
+  predicate: (value: Item<T>) => boolean
+}
 
 export default class Collection<T> {
   items: Map<T, Item<T>> = new Map()
-
-  subsetsByPredicate: Map<InputPredicate<T>, ObservableSet<T>> = new Map()
-  predicatesByInput: Map<InputPredicate<T>, ItemPredicate<T>> = new Map()
+  registrations: Map<InputPredicate<T>, Registration<T>> = new Map()
 
   addFilter(
     predicate: InputPredicate<T>,
     dependencies: InputPredicate<T>[] = []
   ) {
-    // get the ItemPredicates whose values this predicate needs
-    const itemPredicates = dependencies.map((predicate) => {
-      const itemPredicate = this.predicatesByInput.get(predicate)
-      if (itemPredicate === undefined) {
-        throw new Error("Couldn't find ItemPredicate for InputPredicate")
+    // get the Registrations whose values this predicate needs
+    const registrations = dependencies.map((predicate) => {
+      const registration = this.registrations.get(predicate)
+      if (registration === undefined) {
+        throw new Error("Couldn't find Registration for InputPredicate")
       }
 
-      return itemPredicate
+      return registration
     })
 
     // make an ItemPredicate out of this
     const _predicate = (item: Item<T>) => {
-      const filterValues = itemPredicates.map((itemPredicate) =>
-        item.props.get(itemPredicate)?.get()
+      const filterValues = registrations.map((registration) =>
+        item.props.get(registration.predicate)?.get()
       ) as boolean[]
 
       return predicate(item._item, filterValues)
     }
 
-    // associate it with the InputPredicate
-    this.predicatesByInput.set(predicate, _predicate)
-
-    // create a result set
-    const set = new ObservableSet<T>()
-    this.subsetsByPredicate.set(predicate, set)
+    // register the new predicate and its result set
+    const registration: Registration<T> = {
+      set: new ObservableSet<T>(),
+      predicate: _predicate,
+    }
+    this.registrations.set(predicate, registration)
 
     // tell each item to track this predicate
     this.items.forEach((item) => {
-      item.addFilterProp(set, _predicate)
+      item.addFilterProp(registration.set, registration.predicate)
     })
 
-    return set
+    return registration.set
   }
 
   add(item: T) {
@@ -53,9 +54,8 @@ export default class Collection<T> {
       const _item = new Item(item)
       this.items.set(_item.item, _item)
 
-      this.subsetsByPredicate.forEach((set, predicate) => {
-        const _predicate = this.predicatesByInput.get(predicate)!
-        _item.addFilterProp(set, _predicate)
+      this.registrations.forEach((registration) => {
+        _item.addFilterProp(registration.set, registration.predicate)
       })
     })
   }
@@ -63,15 +63,15 @@ export default class Collection<T> {
   remove(item: T) {
     transaction(() => {
       this.items.delete(item)
-      this.subsetsByPredicate.forEach((set, predicate) => {
-        set.delete(item)
+      this.registrations.forEach((registration) => {
+        registration.set.delete(item)
       })
     })
   }
 
   filter(predicate: InputPredicate<T>, dependencies: InputPredicate<T>[] = []) {
-    const set = this.subsetsByPredicate.get(predicate)
-    if (set) return set
+    const registration = this.registrations.get(predicate)
+    if (registration) return registration.set
 
     return this.addFilter(predicate, dependencies)
   }
